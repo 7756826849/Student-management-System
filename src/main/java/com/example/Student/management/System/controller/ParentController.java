2b39a3c4b99ae87e816c4d9f179db1769075de2b
anisabargir24@gmail.com <anisabargir24@gmail.com>
2022-03-17 18:22:42 +0530

    changes

---
+++ src/main/java/com/example/Student/management/System/controller/ParentController.java
@@ -0,0 +1,5 @@
+package com.example.Student.management.System.controller;
+
+public class ParentController {
+
+}
---
+++ src/main/java/com/example/Student/management/System/entity/Parent.java
@@ -0,0 +1,5 @@
+package com.example.Student.management.System.entity;
+
+public class Parent {
+
+}
